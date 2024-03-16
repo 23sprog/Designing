2917ca96a3eda05ae361a4ec4ba2a1b5a41ad761
23sprog <ezatierfan2@gmail.com>
2024-03-16 22:25:47 +0330

    add mini calculator and search box

---
+++ jsProjects/miniCalculator/miniCalculator.js
@@ -0,0 +1,32 @@
+let firstNumber = Number(prompt("Enter first number : "))
+let secondsNumber = Number(prompt("Enter seconds number : "))
+let operator = prompt("Enter operator : ")
+let answer
+
+
+while (operator == ) {
+    
+}
+if(operator === "+"){
+    answer = firstNumber + secondsNumber
+        alert(answer)
+}else if(operator === "-"){
+    answer = firstNumber - secondsNumber
+        alert(answer)
+}else if(operator === "*"){
+    answer = firstNumber * secondsNumber
+        alert(answer)
+}else if(operator === "/"){
+    answer = firstNumber / secondsNumber
+        alert(answer)
+}else if(operator === "**"){
+    answer = firstNumber ** secondsNumber
+        alert(answer)
+}else{
+    alert("عملگر را به درستی وارد کنید !")
+}
+
+if(isNaN(parseInt(answer)))
+{
+    alert("اعداد را به درستی وارد کنید!")
+}
